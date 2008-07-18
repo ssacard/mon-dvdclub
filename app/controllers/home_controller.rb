b3e0b3640819a13abff48e3324b603eef92d0055
sgruhier <sgruhier@mac.com>
2008-07-18 20:29:31 +0200

    debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < AuthenticatedController
+  skip_before_filter :login_required
+  
   def index
     @dvds = self.current_user.dvds.select{|d| d.available?}  
   end
@@ -13,11 +15,13 @@ class HomeController < AuthenticatedController
   
   def reset_db
     system("cd #{RAILS_ROOT}; RAILS_ENV=#{ENV['RAILS_ENV']} rake dvdclub:app:reset")
-    render :text => 'Reset done!!'
+    reset_session
+    redirect_to root_path
   end
   
   def restore_db
     system("cd #{RAILS_ROOT}; mysql -uroot -pk3mr_0ufn -D dvdclub_production < dvdclub_production.sql")
-    render :text => 'Restore done!!'
+    reset_session
+    redirect_to root_path
   end
 end
