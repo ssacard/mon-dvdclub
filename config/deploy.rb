330a5b3997399cd26688b843caa889daefc1d908
sgruhier <sgruhier@mac.com>
2008-12-06 09:43:02 +0100

    update deploy.rb to use github account

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "dvdclub"
+set :application, "mondvdclub"
 
 set :scm,                   :git
 set :deploy_via,            :remote_cache
@@ -7,39 +7,35 @@ set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :scm_verbose, true
 
-set :deploy_to, "/var/www/railsapp/#{application}" 
+set :deploy_to, "/home/deploy/#{application}" 
 set :use_sudo,  false
+set :user, 'deploy'
 
-set :mongrel_rails, '/usr/bin/mongrel_rails'
-set :rake_path,     '/usr/bin/rake'
+set :rake,     '/opt/ruby/bin/rake'
 
-role :web,       'www.xilinus.com'
-role :app,       'www.xilinus.com'
-role :db,        'www.xilinus.com', :primary => true
+role :web,       'www.mondvdclub.fr'
+role :app,       'www.mondvdclub.fr'
+role :db,        'www.mondvdclub.fr', :primary => true
  
-set :repository, "git@git.xilinus.com:/var/git/#{application}"
+set :repository, "git@github.com:Seb20/mon-dvdclub.git"
 set :branch,     'master'
 
 desc "After update code"
 task :after_update_code do
-  %w(database.yml mongrel_cluster.yml).each do |file|
+  %w(database.yml).each do |file|
     run "cp #{shared_path}/resources/#{file} #{release_path}/config/"
   end
 end  
 
 namespace :deploy do
-  task :start, :roles => :app do
-    run "cd #{current_path} && mongrel_rails cluster::start"
+  # Restart passenger on deploy
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
-  task :stop, :roles => :app do
-    run "cd #{current_path} && mongrel_rails cluster::stop"
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
   end
-  task :restart, :roles => :app do
-    run "cd #{current_path} && mongrel_rails cluster::restart"
-  end
-end
-
-
-task :run_test, :roles => :app do
-  run "cd #{current_path} && rake test"
 end
