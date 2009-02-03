f258d926118f2ea7554fa348a277578d185c9096
sgruhier <sgruhier@mac.com>
2009-02-03 20:10:54 +0100

    debug rail 2.3

--- config/initializers/error_message.rb
+++ config/initializers/error_message.rb
@@ -19,24 +19,24 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag
 end    
 
-ActiveRecord::Errors.default_error_messages = {
-  :inclusion => "pas inclus dans la liste",
-  :exclusion => "mot réservé",
-  :invalid => "invalide",
-  :confirmation => "n'est pas égale à la confirmation",
-  :accepted  => "doit être accepter",
-  :empty => "champ obligatoire",
-  :blank => "champ obligatoire",
-  :too_long => "trop long (maximum %d caractères)",
-  :too_short => "trop court (minimum %d caractères)",
-  :wrong_length => "taille incorrecte (doit être de %d caractères)",
-  :taken => "déja pris",
-  :not_a_number => "n'est pas un nombre",
-  :greater_than => "doit être plus grand que %d",
-  :greater_than_or_equal_to => "doit être plus grand ou égal à %d",
-  :equal_to => "doit être plus égale à %d",
-  :less_than => "doit être plus petit que %d",
-  :less_than_or_equal_to => "doit être plus petit ou égal à %d",
-  :odd => "doit être impair",
-  :even => "doit être pair"
-}
+# ActiveRecord::Errors.default_error_messages = {
+#   :inclusion => "pas inclus dans la liste",
+#   :exclusion => "mot réservé",
+#   :invalid => "invalide",
+#   :confirmation => "n'est pas égale à la confirmation",
+#   :accepted  => "doit être accepter",
+#   :empty => "champ obligatoire",
+#   :blank => "champ obligatoire",
+#   :too_long => "trop long (maximum %d caractères)",
+#   :too_short => "trop court (minimum %d caractères)",
+#   :wrong_length => "taille incorrecte (doit être de %d caractères)",
+#   :taken => "déja pris",
+#   :not_a_number => "n'est pas un nombre",
+#   :greater_than => "doit être plus grand que %d",
+#   :greater_than_or_equal_to => "doit être plus grand ou égal à %d",
+#   :equal_to => "doit être plus égale à %d",
+#   :less_than => "doit être plus petit que %d",
+#   :less_than_or_equal_to => "doit être plus petit ou égal à %d",
+#   :odd => "doit être impair",
+#   :even => "doit être pair"
+# }
