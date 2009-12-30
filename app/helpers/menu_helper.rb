a2af5fd14a1b5143b8b0c34ca93eed953e1ba312
Nicolas Jacobeus <jacobeus@Nicolas-Jacobeuss-MacBook-Pro.local>
2009-12-30 15:21:11 +0100

    another weird pgsql problem, this time with named_scope

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -2,9 +2,7 @@ module MenuHelper
   
   # Count the number of actions needed for the red box near the "demande en cours" tab
   def actions_needed
-    awaiting_approval = current_user.owned_dvds.awaiting_approval
-    pending_requests = current_user.pending_requests
-    awaiting_approval.count + pending_requests.count
+    current_user.owned_dvds.awaiting_approval.count + current_user.pending_requests.count
   end
   
 end
