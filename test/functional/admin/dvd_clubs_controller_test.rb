5edf42435b471bc97fef44a86761596fc7e8c570
Nicolas Jacobeus <jacobeus@Nicolas-Jacobeuss-MacBook-Pro.local>
2010-01-25 22:35:42 +0100

    removed outdated test dir

--- test/functional/admin/dvd_clubs_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Admin::DvdClubsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
--- test/unit/club_topic_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ClubTopicTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
