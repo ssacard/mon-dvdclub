236fd558db1b8bf739b22b86c8fea9df3e495791
Sebastien Sacard <sebastien.sacard@gmail.com>
2010-08-25 10:18:00 +0200

    add environmennts conf

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "amazon-ecs", :lib => "amazon/ecs"
   config.gem "haml"
   config.gem "will_paginate"
+  config.gem "mysql"
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
