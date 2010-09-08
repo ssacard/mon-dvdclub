0a596c1d5b24fb3e6614774e4c3757806e4cc91e
Sebastien Tanguy <stanguy@gmail.com>
2010-09-08 17:16:07 +0200

    Correct active status for logout and the admin checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < AuthenticatedController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
   skip_before_filter :login_required, :only => [ :new, :create ]
+
+  # don't check auth, else we won't be able to logout if inactive
+  def check_auth
+    true
+  end
+
   # render new.rhtml
   def new
   end
@@ -10,6 +16,15 @@ class SessionsController < AuthenticatedController
   def create
     self.current_user = User.authenticate(params[:email], params[:password])
     if logged_in?
+      if ! self.current_user.active?
+        self.current_user.forget_me
+        self.current_user = nil
+        cookies.delete :auth_token
+        reset_session
+        flash[:notice] = "Login ou mot de passe incorrect"
+        render :action => 'new'
+        return
+      end
       if params[:remember_me] == "1"
         current_user.remember_me unless current_user.remember_token?
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
