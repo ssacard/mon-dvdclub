47ed318eea85e640aba3198db651ff24ae53bcf9
Sebastien Tanguy <stanguy@gmail.com>
2010-09-21 11:03:58 +0200

    GH-21: Redirect to the list of dvd clubs when destroying a club

--- app/controllers/user_dvd_clubs_controller.rb
+++ app/controllers/user_dvd_clubs_controller.rb
@@ -3,9 +3,9 @@ class UserDvdClubsController < ApplicationController
   make_resourceful do
     actions :all
 
-#    response_for :destroy do
-#      redirect_to dvd_club_path(current_object.dvd_club)
-#    end
+   response_for :destroy do
+     redirect_to dvd_clubs_path
+   end
 #
 #    response_for :create do
 #      redirect_to dvd_club_path(current_object.dvd_club)
