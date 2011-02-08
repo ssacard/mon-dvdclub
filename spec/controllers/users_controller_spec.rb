d633e465bbcc77a454d3a93934f59dbad554ab53
Olivier El Mekki <oelmekki@gmail.com>
2011-02-08 22:17:43 +0100

    ADD missing tests for users_controller#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,25 +5,148 @@ require File.dirname(__FILE__) + '/../spec_helper'
 include AuthenticatedTestHelper
 
 describe UsersController do
-  describe 'POST create' do
-    before :each do
-      User.delete_all
-      DvdClub.delete_all
-      Invitation.delete_all
+  before :each do
+    User.delete_all
+    DvdClub.delete_all
+    Invitation.delete_all
 
-      @valid_user_attrs = {
-        :login => 'quire', 
-        :email => 'quire@example.com',
-        :email_confirmation => 'quire@example.com',
-        :password => 'quire', 
-        :password_confirmation => 'quire' 
-      }
+    @valid_user_attrs = {
+      :login => 'quire', 
+      :email => 'quire@example.com',
+      :email_confirmation => 'quire@example.com',
+      :password => 'quire', 
+      :password_confirmation => 'quire' 
+    }
 
-      @valid_dvdclub_attrs = {
-        :name => 'Sample club'
-      }
+    @valid_dvdclub_attrs = {
+      :name => 'Sample club'
+    }
+  end
+
+  describe 'GET new' do
+    describe 'with new user' do
+      describe 'without invitation' do
+        before :each do
+          get :new
+        end
+
+        it 'should assign new user' do
+          assigns[ :user ].new_record?.should be_true
+        end
+
+        it 'should assign new dvdclub' do
+          assigns[ :dvd_club ].new_record?.should be_true
+        end
+
+        it 'should render "new" template' do
+          response.should render_template( :new )
+        end
+      end
+
+      describe 'with invitation' do
+        before :each do
+          @user1 = User.create! @valid_user_attrs
+          @dvd_club = DvdClub.create! @valid_dvdclub_attrs.merge( :owner_id => @user1.id )
+
+          UserDvdClub.create! :invited_by          => nil,
+                              :user_id             => @user1.id,
+                              :dvd_club_id         => @dvd_club.id,
+                              :subscription_status => true
+
+          Invitation.create! :dvd_club_id => @dvd_club.id, 
+                             :user_id     => @user1.id, 
+                             :token       => 'test', 
+                             :email       => 'user2@example.com',
+                             :active      => true
+
+          get :new, :token => 'test'
+        end
+
+        it 'should assign new user with prefill email' do
+          assigns[ :user ].new_record?.should be_true
+          assigns[ :user ].email.should == 'user2@example.com'
+        end
+
+        it 'should retrieve dvd_club' do
+          assigns[ :dvd_club ].should == @dvd_club
+        end
+
+        it 'should render "new" template' do
+          response.should render_template( :new )
+        end
+      end
     end
 
+    describe 'with already registered user' do
+      describe 'when not joined' do
+        before :each do
+          @user1 = User.create! @valid_user_attrs
+          @user2 = User.create! @valid_user_attrs.merge( :login => 'user2', :email => 'user2@example.com' )
+
+          @dvd_club = DvdClub.create! @valid_dvdclub_attrs.merge( :owner_id => @user1.id )
+
+          UserDvdClub.create! :invited_by          => nil,
+                              :user_id             => @user1.id,
+                              :dvd_club_id         => @dvd_club.id,
+                              :subscription_status => true
+
+          Invitation.create! :dvd_club_id => @dvd_club.id, 
+                             :user_id     => @user1.id, 
+                             :token       => 'test', 
+                             :email       => @user2.email,
+                             :active      => true
+
+          get :new, :token => 'test'
+        end
+
+        it 'should assign user' do
+          assigns[ :user ].new_record?.should be_false
+          assigns[ :user ].should == @user2
+        end
+
+        it 'should retrieve dvd_club' do
+          assigns[ :dvd_club ].should == @dvd_club
+        end
+
+        it 'should render "join" template' do
+          response.should render_template( 'dvd_clubs/join' )
+        end
+      end
+
+      describe 'when already joined' do
+        before :each do
+          @user1 = User.create! @valid_user_attrs
+          @user2 = User.create! @valid_user_attrs.merge( :login => 'user2', :email => 'user2@example.com' )
+
+          @dvd_club = DvdClub.create! @valid_dvdclub_attrs.merge( :owner_id => @user1.id )
+
+          UserDvdClub.create! :invited_by          => nil,
+                              :user_id             => @user1.id,
+                              :dvd_club_id         => @dvd_club.id,
+                              :subscription_status => true
+
+          UserDvdClub.create! :invited_by          => nil,
+                              :user_id             => @user2.id,
+                              :dvd_club_id         => @dvd_club.id,
+                              :subscription_status => true
+
+          Invitation.create! :dvd_club_id => @dvd_club.id, 
+                             :user_id     => @user1.id, 
+                             :token       => 'test', 
+                             :email       => @user2.email,
+                             :active      => true
+
+          get :new, :token => 'test'
+        end
+
+        it 'should redirect to home page' do
+          response.should redirect_to( home_path )
+        end
+      end
+    end
+  end
+
+  describe 'POST create' do
     describe 'without invitation' do
       describe 'with valid user and dvd_club' do
         before :each do
