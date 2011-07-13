34a5ed6de14098f91151135c63c206ab82a4fea0
Olivier El Mekki <oelmekki@gmail.com>
2011-07-13 14:37:52 +0200

    CHANGE switch gem manager to bundler

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '2.3.5'
+gem 'rake', '0.8.7'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'amazon-ecs'
+gem 'haml'
+gem 'ruby-hmac'
+gem 'will_paginate'
+
+gem 'yajl-ruby'
+gem 'facebooker2'
+
+group :development, :test do
+  gem 'rspec-rails', '1.2'
+  gem 'cucumber'
+  gem 'capybara', '0.3.9'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,12 @@
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
+#
+require 'thread'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
