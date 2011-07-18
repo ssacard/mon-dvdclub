8b2fbd23a8832637e5d85ff1466e566365fdd74f
Sebastien Sacard <sebastien.sacard@gmail.com>
2011-07-18 12:22:55 +0200

    fix change sendgrid email account
    Signed-off-by: Sebastien Sacard <sebastien.sacard@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => "smtp.sendgrid.net",
   :port           => "25",
   :authentication => :plain,
-  :user_name      => 'seb.deuxzero@gmail.com',
+  :user_name      => 'ssacard',
   :password       => 'pemdss3nd',
   :domain         => 'mondvdclub.fr',
 }
