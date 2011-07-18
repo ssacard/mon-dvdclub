254e36d79aed827ddc305866570ce067177367ac
Sebastien Sacard <sebastien.sacard@gmail.com>
2011-07-18 16:46:46 +0200

    small fixes on emails (type, formating)
    Signed-off-by: Sebastien Sacard <sebastien.sacard@gmail.com>

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
     content_type "text/html"
     recipients    user.email
     from          "noreply@mondvdclub.fr"
-    subject       "Bienvenue sur MonDVDCub.fr"
+    subject       "Bienvenue sur MonDVDClub.fr"
     body          :body => {:name => user.login, :home_url => home_url}
   end
 
